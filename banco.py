904c83116040e0b56de782473ab92420d290c320
gustavo0177 <gustavodearaujogarciagu@gmail.com>
2024-10-10 17:50:32 -0300

    commit
    tive que fazer outro

--- banco.py
+++ banco.py
@@ -1,25 +1,28 @@
 senha=input("informe a senha")
 
 if senha == "1234":
- print("/n1.extrato")
-print("2. deposito")
+ print("1.extrato")
+print("2.deposito")
 print("3.saque")
 print("4.sair")
 opcao = input("escolha uma opçao")
 
 if opcao == "1":
- print("saldo: R$ 2000")
+ print("saldo: R$ 1000")
 
-if opcao == "2":
+elif opcao == "2":
  valor = input("o valor do deposito:R$") 
  print(f"voce depositou R${valor}")
 
-if opcao == "3":
- valor = input("o valor do saque:R$") 
+elif opcao == "3":
+ valor = input("o valor do saque:R$")
 print(f"voce sacou R${valor}")
- 
+
 if opcao == "4":
  print("saindo..")
+
 else:
  print("opçao invalida")
+
+
 
